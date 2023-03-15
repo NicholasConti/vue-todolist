e5e68b835842f67bbedab06ce2304dfa4094b250
NicholasConti <conti94@live.it>
2023-03-15 16:29:39 +0100

    milestone3

--- js/mian.js
+++ js/mian.js
@@ -3,26 +3,27 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            newTodo:'',
             myTodo: [
                 {
                     text: 'todo1',
-                    done: true,
+                    done: true
                 },
                 {
                     text: 'todo2',
-                    done: false,
+                    done: false
                 },
                 {
                     text: 'todo3',
-                    done: false,
+                    done: false
                 },
                 {
                     text: 'todo4',
-                    done: false,
+                    done: false
                 },
                 {
                     text: 'todo5',
-                    done: false,
+                    done: false
                 }
             ]
         }
@@ -31,6 +32,17 @@ createApp({
         // FUNZIONE PER ELIMINARE GLI ELEMENTI DELLA LISTA AL CLICK DEL CESTINO
         deleteTodo(indexTodo){
             this.myTodo.splice(indexTodo, 1);
+        },
+        // FUNZIONE PER AGGIUNGERE UN ELEMENTO ALLA LISTA
+        addTodo(){
+            if(this.newTodo.length > 0){
+                this.robanuova = {
+                    text: this.newTodo,
+                    done: false
+                }
+                this.myTodo.push(this.robanuova);
+                this.newTodo = '';
+            }
         }
     }
 }).mount('#app');
